79badde6222b6dfa5a4bd5b94ac641b99f5b30a9
Wanjiru Mburu <wanjirumarionne@gmail.com>
2024-02-13 10:38:51 +0300

    Completed Putting it All Together: React Fetch CRUD Lab

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,9 +1,36 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 function QuestionList() {
+
+  // const [selectedCategory, setSelectedCategory] = useState([])
+  const [question, setQuestions] = useState([]); 
+ useEffect(() =>{
+  fetch('  http://localhost:4000/questions')
+  .then((r)=> r.json())
+  .then((question)=> setQuestions(question))
+ },[]) 
+
+ function handleDeletedQuestion(deletedQuestion){
+  console.log("done", deletedQuestion);
+  const updateQuestions = question && question.filter((q) =>  
+  q.id !== deletedQuestion);
+  setQuestions(updateQuestions);
+ }
+
+
   return (
     <section>
-      <h1>Quiz Questions</h1>
+      <h1>
+        Quiz Questions
+        {question &&
+          question.map((question) => (
+            <QuestionItem
+              key={question.id}
+              question={question}
+              onDeleteItem={handleDeletedQuestion}
+            />
+          ))}
+      </h1>
       <ul>{/* display QuestionItem components here after fetching */}</ul>
     </section>
   );
